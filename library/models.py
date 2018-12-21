0c838909846c5383dab1059db04af6ac6116feac
Kwaidan00 <aleksander.spyra@gmail.com>
2018-12-21 13:43:51 +0100

    Admin pages was registered

--- library/models.py
+++ library/models.py
@@ -8,7 +8,13 @@ class Author(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return '{} {}'.format(self.first_name, self.last_name)
+
 
 class Book(models.Model):
     title = models.CharField(max_length=300)
     author = models.ForeignKey('library.Author', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return '{}'.format(self.title)
